4e3cadc5b2caed8fd829723c0c5f9e946c716339
Tristan Brown <tgbrown@stanford.edu>
2018-09-13 06:37:38 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,14 +8,15 @@ attr_accessor :name
     @@all << self
   end
 
-  def self.all
-    @@all.each do |dog|
-      puts dog.name
-  end
-end
 
 def self.clear_all
   @@all.clear
+end
+
+def self.all
+  @@all.each do |dog|
+    puts dog.name
+end
 end
 
 end
